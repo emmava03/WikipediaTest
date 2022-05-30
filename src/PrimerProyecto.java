7297843e1d3f11a713b7745028508ea859706bb6
Emmanuel Volpi Aranda <106482431+emmava03@users.noreply.github.com>
2022-05-30 14:43:10 -0300

    Primer Commit

---
+++ src/PrimerProyecto.java
@@ -0,0 +1,26 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class PrimerProyecto {
+
+	@Test(description = "Validar que las busquedas en Wikipedia funcionan")
+	public void ValidarBusquedaWikipedia() throws Exception {
+		System.setProperty("webdriver.chrome.driver", "C:\\chromedriver_win32\\\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("http://wikipedia.org");
+		WebElement searchInput = driver.findElement(By.id("searchInput"));
+		Assert.assertTrue(searchInput.isDisplayed());
+		searchInput.sendKeys("Selenium");
+		searchInput.submit();
+		WebElement tituloResultado = driver.findElement(By.id("firstHeading"));
+		System.out.println("Texto encontrado " + tituloResultado.getText());
+		Assert.assertTrue(tituloResultado.isDisplayed());
+		driver.close();
+
+	}
+
+}
